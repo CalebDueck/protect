f503da25ccc59bda5107368f16949cf3dd902bc0
activateMotor <activateMotor>
2024-01-20 16:06:48 -0500

    added sudo and venv activation for pygame run script + sidewall button integration with game

--- server/SideWall.py
+++ server/SideWall.py
@@ -25,7 +25,7 @@ class SideWallMainApp(BaseServerGame):
             button = Button(_id, press_pin, led_pin)
             button.set_callback(button_click_callback)
             button.led.turn_off_leds()
-            self.buttons.append([button, False, c.no_light])
+            self.buttons.append([button, False, c.no_light, 0])
             _id += 1
 
       
@@ -47,14 +47,22 @@ class SideWallMainApp(BaseServerGame):
                     continue
                 elif event.type == ActionEvents.BUTTON_CLICK_SIDEWALL:
                     button_id = event.dict['message']['id']
-                    self.buttons[button_id][0].led.set_button_color(color=c.white)
-
-                    print("set color to white")
-                    break
+                    button = self.buttons[button_id]
+                    if (button[3] > 0 and button[1] == False):
+                        button[1] = True
+                        self.points = button[3]
+                        button[3] = 0
+                        button[2] = c.green
+                        button[0].led.set_button_color(color=c.green)
+                    self.server.point_update(points=self.points,lives=self.lives)
+                    self.points = 0
+                    self.lives = 0
+                    print("Sending Client Update")
                 
 
             self.extractCustomEvents()
-
+            
+            self.update_information()
 
             # Update the display
             pygame.display.flip()
@@ -63,13 +71,68 @@ class SideWallMainApp(BaseServerGame):
             pygame.time.Clock().tick(30)
 
     def read_game_file(self):
-        with open('backWallClickGame.json', 'r') as file:
-            self.game_data = json.load(file)   
+        with open('side_wall_game.json', 'r') as file:
+            self.game_data = json.load(file)
+            
+    def update_information(self):
+        if self.game_data == None:
+            return
+        
+        if self.start_game == False:
+            return
+        
+        levels = self.game_data.get("levels", [])
+        target_level = next((level for level in levels if level["level_id"] == self.level), None)
+        commands = target_level['commands']
+        time_since_start = time.time() - self.start_time
+  
+        for command in commands:
+            time_start = command['time_start']
+
+            if time_since_start < time_start:
+                 continue          
+            
+            
+            command_id = command['command_id']
+            command_name = command['command_name']
+            button_id = command['button_id']
+            points = command['points']
+            time_end = command['time_end']
+            button = self.buttons[button_id]
+            colour = command['colour']
+            if time_since_start > time_end and not 'completed' in command:
+                
+                if button[1] == False and button[3] > 0:
+                    self.lives = -1
+                    self.server.point_update(self.points,self.lives)
+                    self.lives = 0
+                    button[1] = False
+                    button[2] = c.no_light
+                    button[3] = 0
+                    button[0].led.set_button_color(color=c.no_light)
+                else:
+                    if button[1] == True:
+                        button[1] = False
+                        button[2] = c.no_light
+                        button[3] = 0
+                        button[0].led.set_button_color(color=c.no_light)
+
+                command['completed'] = True
+                
+            elif not 'completed' in command:
+                if button[1] != True:
+                    if colour == 'R' and button[3] == 0:
+                        button[0].led.set_button_color(color=c.red)
+                    elif colour == 'B' and button[3] == 0:
+                        button[0].led.set_button_color(color=c.blue)
+                    
+                    button[3] = points
+
 
                
 
 if __name__ == "__main__":
-    buttons = [(17,18)]
+    buttons = [(17,12)]
     sideWallMainApp = SideWallMainApp(800, 600, buttons,'activateMotor.local',12345)
     sideWallMainApp.connect_client()
     sideWallMainApp.run()
